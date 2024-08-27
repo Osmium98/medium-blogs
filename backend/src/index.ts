64022718ec7651420450b6bb552c9e5398de70f3
Osmium98 <subhammallik553@gmail.com>
2024-08-28 00:02:10 +0530

    dt-27/08/2024

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/extension';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { Jwt } from 'hono/utils/jwt';
+import { sign } from 'hono/jwt';
 
 
 const app = new Hono<{
@@ -17,16 +19,22 @@ app.post('/api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate())
-  return c.text('Hello Hono!')
-
+  
   const body = await c.req.json();
 
-  await prisma.user.create({
+  const user = await prisma.user.create({
     data:{
       email: body.email,
       password: body.password
     }
 
+  })
+
+  const token = sign({id:user.id},"secret",);
+
+
+  return c.json({
+    jwt:token
   })
 
 })
