371efc54d2b31ac39d8508d00351e885dbde083d
Osmium98 <subhammallik553@gmail.com>
2024-08-30 23:25:27 +0530

    dt-30/8/2024

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,8 @@ import { sign } from 'hono/jwt';
 
 const app = new Hono<{
   Bindings:{
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
 }>()
 
@@ -30,7 +31,7 @@ app.post('/api/v1/signup', async (c) => {
 
   })
 
-  const token = sign({id:user.id},"secret",);
+  const token = sign({id:user.id},c.env.JWT_SECRET);
 
 
   return c.json({
@@ -38,8 +39,30 @@ app.post('/api/v1/signup', async (c) => {
   })
 
 })
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signin',async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where:{
+      email:body.email
+    }
+  });
+
+  if(!user){
+    c.status(403);
+    return c.json({
+      error:"user not found"
+    })
+  }
+
+  const jwt = await sign({id:user.id},c.env.JWT_SECRET)
+
+  return c.json({
+    jwt:
+  })
 
 })
 app.post('/api/v1/blog', (c) => {
