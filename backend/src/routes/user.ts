1e7b55108633cae6b840c101878764fe6b0a56ea
Osmium98 <subhammallik553@gmail.com>
2024-09-04 23:24:53 +0530

    dt:4/4/24 zod

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign, verify } from 'hono/jwt';
+import { signupInput } from "./zod";
 
 
 export const userRouter = new Hono<{
@@ -18,6 +19,13 @@ userRouter.post('/signup', async (c) => {
     }).$extends(withAccelerate())
 
     const body = await c.req.json();
+    const {success} = signupInput.safeParse(body);
+    if(!success){
+        c.status(411);
+        return c.json({
+            message:"Input not correct"
+        })
+    }
     try {
         const user = await prisma.user.create({
             data: {
